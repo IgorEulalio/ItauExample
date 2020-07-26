5f9ed24f2bc8beede1218d1a140ae52b9f34983c
IgorEulalio <igoreulalio.ie@gmail.com>
2020-07-26 18:39:45 -0300

    criando projeto de exemplo para esteira de testes

---
+++ src/main/java/com/example/itau/Projeto/Itau/para/apresenta/o/da/esteira/de/testes/Application.java
@@ -0,0 +1,13 @@
+package com.example.itau.Projeto.Itau.para.apresenta.o.da.esteira.de.testes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/itau/Projeto/Itau/para/apresenta/o/da/esteira/de/testes/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.itau.Projeto.Itau.para.apresenta.o.da.esteira.de.testes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
